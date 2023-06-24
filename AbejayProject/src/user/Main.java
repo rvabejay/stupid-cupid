9850274d2d8a14f27696f6d7816662665452e993
Rhys Allen Abejay <137622245+rvabejay@users.noreply.github.com>
2023-06-24 22:33:06 +0800

    Add files via upload

---
+++ AbejayProject/src/user/Main.java
@@ -0,0 +1,18 @@
+package user;
+
+import game.GameStage;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage stage){
+		GameStage theGameStage = new GameStage();
+		theGameStage.setStage(stage);
+	}
+
+}
